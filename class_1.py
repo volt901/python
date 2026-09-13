066157e495ade8d345e8211a91ef37278998803f
vladimir <volt901@icloud.com>
2026-09-13 21:39:36 +0300

    five commit

--- class_1.py
+++ class_1.py
@@ -1,10 +1,25 @@
 class Product:
-    def __init__(self, name:str, price:int, stok:int, categoria:Category):
+    def __init__(self, name:str, price:int, stock:int, categoria:Category):
+        if price <= 0:
+            raise ValueError("Ошибка ввода: указать цену больше 0")
+        if stock < 0:
+            raise ValueError("Ошибка ввода: значения остатка не может быть меньше 0")
+
         self.name = name
         self.price = price
-        self.stok = stok
+        self.stock = stock
         self.categoria = categoria
-    ...
+
+    def sell(self, quantity:int)-> int:
+        if quantity <= 0:
+            raise ValueError("Ошибка ввода: указать количество проданного товара больше 0")
+
+        if quantity > self.stock:
+            raise ValueError(f"Ошибка ввода: недостаточно товара на складе, на складе: {self.stock}, запросили: {quantity}")
+
+        self.stock -= quantity
+        takings = self.price * quantity
+        return takings
 
 class Category:
     def __init__(self, name:str, description=""):
@@ -16,6 +31,9 @@ class Category:
         self.__product.append(product)
 
 food = Category("food")
-fish = Product("fish", 10, 100, food)
+fish = Product("fish", 99, 10, food)
+print(fish.stock)
+fish.sell(10)
+print(fish.stock)
 
-print(fish.categoria.name)
+print(fish.stock)
